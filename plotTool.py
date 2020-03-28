e104842ff3710ee4518dd411e62c7e159fd91c56
915615783 <915615783@qq.com>
2020-03-29 01:51:15 +0800

    change some params

--- plotTool.py
+++ plotTool.py
@@ -25,7 +25,7 @@ def plot2D(support_emb, query_emb, label, N, K, total_Q, hidden_size, plot_num=2
             plt.scatter(support[n, :, 0], support[n, :, 1], c=colors[n], marker='^')
         # plot query
         plt.scatter(query[:, 0], query[:, 1], c=[colors[label[q]] for q in range(total_Q)], marker='.')
-        plt.savefig('%d.png'%figcount, dpi=300)
+        plt.savefig('image/%d.png'%figcount, dpi=300)
         figcount += 1
         plt.show()
 
